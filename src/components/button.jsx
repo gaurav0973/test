ff8f341708ac00fbdb56dbae3e316c2c899a43b1
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-22 11:43:08 +0530

    buttom component added

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+export default function Button() {
+    return (
+        <div>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                Click Me
+            </button>
+        </div>
+    );
+}
