b5fb6c6987bd18e1a92c10c5577f34be3ceba4b2
gaurav0973 <gauravKmaurya09@gmail.com>
2026-06-24 13:06:03 +0530

    delpoy

---
+++ src/components/input.jsx
@@ -0,0 +1,11 @@
+export default function Input() {
+    return (
+        <div>
+            <input
+                type="text"
+                className="border border-gray-300 px-4 py-2 rounded-md"
+                placeholder="Enter text"
+            />
+        </div>
+    );
+}
